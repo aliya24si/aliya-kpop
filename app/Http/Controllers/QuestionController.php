019e96452028e1f17602e1a544a18d565b1ac4f7
aliya24si <safwa24si@mahasiswa.pcr.ac.id>
2025-09-29 10:39:10 +0700

    penambahan eror pada form

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -26,6 +26,16 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
+
+        $request->validate([
+            'nama'       => 'required|max:10',
+            'email'      => ['required', 'email'],
+            'pertanyaan' => 'required|max:300|min:8',
+        ], [
+            'nama.required' => 'Nama tidak boleh kosong',
+            'email.email'   => 'Email Tidak valid',
+        ]);
+
         //dd($request->all());
         $data['nama']       = $request->nama;
         $data['email']      = $request->email;
